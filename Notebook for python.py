8f71bb2c770f7ef2fe4579516ef8370f0e9c3a59
rafagardo <92759418+rafagardo@users.noreply.github.com>
2022-06-26 11:42:13 -0500

    Jupyter Notebook from IBM Cloud
    This Jupyter notebook is the one exported from IBM Cloud

---
+++ Notebook for python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This Notebook is for all the activities done in IBM Data Science certification
+
+# Before starting to code, insert a Markdown cell above in order to know what´s going on. For example:
+
+# The sum by two numbers is given by:
+
+# In[1]:
+
+
+print(1+2)
+
